7c5f41eaf13f6cbd1e5ffaf67b6dfe3f9460e6c8
shadowaxe99 <128367359+shadowaxe99@users.noreply.github.com>
2023-09-22 19:11:02 -0400

    Reformat code

--- emailer-assistant/calendar_api.py
+++ emailer-assistant/calendar_api.py
@@ -9,21 +9,25 @@ import pickle
 
 # Initialize the Calendar API
 
+
 def get_calendar_service():
     creds = None
     if creds and not creds.valid:
         if creds.expired and creds.refresh_token:
             creds.refresh(Request())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', ['https://www.googleapis.com/auth/calendar'])
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "REDACTED", ["https://www.googleapis.com/auth/calendar"]
+        )
         creds = flow.run_local_server(port=0)
-        with open('calendar_token.json', 'w') as token:
+        with open("calendar_token.json", "w") as token:
             token.write(creds.to_json())
 
-    return build('calendar', 'v3', credentials=creds)
+    return build("calendar", "v3", credentials=creds)
 
 
 # Fetch free time slots for the next two months
+
 
 def fetch_free_time(calendar_service, email_address):
     now = datetime.utcnow()
@@ -31,21 +35,21 @@ def fetch_free_time(calendar_service, email_address):
 
     # Fetch the free/busy information
     body = {
-        "timeMin": now.isoformat() + 'Z',
-        "timeMax": end_time.isoformat() + 'Z',
-        "items": [{"id": email_address}]
+        "timeMin": now.isoformat() + "Z",
+        "timeMax": end_time.isoformat() + "Z",
+        "items": [{"id": email_address}],
     }
     free_busy_response = calendar_service.freebusy().query(body=body).execute()
 
     # Extract the busy times
-    busy_times = free_busy_response['calendars'][email_address]['busy']
+    busy_times = free_busy_response["calendars"][email_address]["busy"]
 
     # Calculate the free times based on the busy times
     free_slots = []
     prev_end_time = now
     for busy in busy_times:
-        busy_start = datetime.fromisoformat(busy['start'][:-1])
-        busy_end = datetime.fromisoformat(busy['end'][:-1])
+        busy_start = datetime.fromisoformat(busy["start"][:-1])
+        busy_end = datetime.fromisoformat(busy["end"][:-1])
         if busy_start != prev_end_time:
             free_slots.append((prev_end_time, busy_start))
         prev_end_time = busy_end
@@ -56,7 +60,7 @@ def fetch_free_time(calendar_service, email_address):
     # Group free slots by day and format them
     free_slots_by_day = {}
     for start, end in free_slots:
-        day = start.strftime('%Y-%m-%d')
+        day = start.strftime("%Y-%m-%d")
         time_range = f"{start.strftime('%H:%M')} - {end.strftime('%H:%M')}"
         if day in free_slots_by_day:
             free_slots_by_day[day].append(time_range)
@@ -66,15 +70,15 @@ def fetch_free_time(calendar_service, email_address):
     # Convert the grouped free slots into a human-readable format
     formatted_free_slots = []
     for day, time_ranges in free_slots_by_day.items():
-        formatted_day = datetime.fromisoformat(day).strftime('%A, %d %B %Y')
-        formatted_time_ranges = ', '.join(time_ranges)
+        formatted_day = datetime.fromisoformat(day).strftime("%A, %d %B %Y")
+        formatted_time_ranges = ", ".join(time_ranges)
         formatted_free_slots.append(f"{formatted_day}: {formatted_time_ranges}")
 
-    return '\n'.join(formatted_free_slots)
+    return "\n".join(formatted_free_slots)
 
 
-if __name__ == '__main__':
-    with open('calendar_service.pkl', 'rb') as f:
-            calendar_service = pickle.load(f)
+if __name__ == "__main__":
+    with open("calendar_service.pkl", "rb") as f:
+        calendar_service = pickle.load(f)
 
     print(fetch_free_time(calendar_service, "shivammittal2124@gmail.com"))
--- emailer-assistant/services.py
+++ emailer-assistant/services.py
@@ -6,32 +6,36 @@ from google.auth.transport.requests import Request
 def get_services():
     # Load or authorize Gmail credentials
     gmail_creds = None
-    gmail_scopes = ['https://www.googleapis.com/auth/gmail.modify']
+    gmail_scopes = ["https://www.googleapis.com/auth/gmail.modify"]
 
     if gmail_creds and not gmail_creds.valid:
         if gmail_creds.expired and gmail_creds.refresh_token:
             gmail_creds.refresh(Request())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', gmail_scopes)
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", gmail_scopes
+        )
         gmail_creds = flow.run_local_server(port=0)
-        with open('gmail_token.json', 'w') as token:
+        with open("gmail_token.json", "w") as token:
             token.write(gmail_creds.to_json())
 
     # Load or authorize Calendar credentials
     calendar_creds = None
-    calendar_scopes = ['https://www.googleapis.com/auth/calendar']
+    calendar_scopes = ["https://www.googleapis.com/auth/calendar"]
 
     if calendar_creds and not calendar_creds.valid:
         if calendar_creds.expired and calendar_creds.refresh_token:
             calendar_creds.refresh(Request())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', calendar_scopes)
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", calendar_scopes
+        )
         calendar_creds = flow.run_local_server(port=0)
-        with open('calendar_token.json', 'w') as token:
+        with open("calendar_token.json", "w") as token:
             token.write(calendar_creds.to_json())
 
     # Build the services
-    gmail_service = build('gmail', 'v1', credentials=gmail_creds)
-    calendar_service = build('calendar', 'v3', credentials=calendar_creds)
+    gmail_service = build("gmail", "v1", credentials=gmail_creds)
+    calendar_service = build("calendar", "v3", credentials=calendar_creds)
 
     return gmail_service, calendar_service
